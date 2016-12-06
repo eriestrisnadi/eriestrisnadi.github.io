0dd140f85289a14e26589709c1d58727f3293182
Eries Trisnadi <zdumb1885@outlook.com>
2016-12-06 14:06:06 +0700

    remove amp, update copy task, add navbar for resume

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,7 +20,8 @@ module.exports = function(grunt) {
               {expand: true, cwd: 'node_modules/bootstrap/dist/js/', src: ['**/*.min.js'], dest: 'dist/js/'},
               {expand: true, cwd: 'node_modules/bootstrap/dist/css/', src: ['**/*.min.css'], dest: 'dist/css/'},
               {expand: true, cwd: 'node_modules/daemonite-material/js/', src: ['**/*.min.js'], dest: 'dist/js/'},
-              {expand: true, cwd: 'node_modules/daemonite-material/css/', src: ['**/*.min.css'], dest: 'dist/css/'}
+              {expand: true, cwd: 'node_modules/daemonite-material/css/', src: ['**/*.min.css'], dest: 'dist/css/'},
+              {expand: true, cwd: 'src/resume/', src: ['resume.pdf'], dest: 'resume/'}
             ],
           },
         },
