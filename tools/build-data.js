b8f3bcc9b370e57165139b10677fbb113e1c1851
Eries Trisnadi <lognus21@gmail.com>
2020-09-17 15:50:26 +0700

    chore: update automated build tools, remove dep deepmerge, add deps lodash & prettier
    Signed-off-by: Eries Trisnadi <lognus21@gmail.com>

--- tools/build-data.js
+++ tools/build-data.js
@@ -1,12 +1,11 @@
 const { writeFileSync, readdirSync, readFileSync } = require('fs');
 const { join, resolve } = require('path');
-const merge = require('deepmerge')
+const { set, values, merge, keyBy } = require('lodash');
 const { name, version, author } = require('../package.json');
 const RESOURCES_PATH = resolve('resources');
 const JSON_DATA_PATH = join(RESOURCES_PATH, 'data-json');
 const JSON_INDEX_PATH = join(RESOURCES_PATH, 'index.json');
 const JSON_DATA = readdirSync(JSON_DATA_PATH);
-
 
 const SETTING_DATA = {
   setting: {
@@ -16,22 +15,34 @@ const SETTING_DATA = {
   },
 };
 
-let DATA = {};
-
-JSON_DATA.map(o => {
-  const item = {
-    name: o.replace('.json', ''),
-    data: JSON.parse(readFileSync(join(JSON_DATA_PATH, o), 'utf8') || '{}'),
-  };
-  
-  if(item.name != 'setting') {
-    DATA[item.name] = item.data;
-  } else {
-    SETTING_DATA[item.name] = merge(SETTING_DATA[item.name], item.data);
+const NAVIGATION_DATA = [
+  {
+    name: "Contact",
+    icon: "mail",
+    link: `mailto:${author.email || ''}`,
   }
-  return o;
-});
+];
 
-DATA = merge(SETTING_DATA, DATA);
+const DATA = JSON_DATA
+  .map(obj => ({
+    key: obj.replace('.json', ''),
+    value: JSON.parse(readFileSync(join(JSON_DATA_PATH, obj), 'utf8') || '{}')
+  }))
+  .reduce((obj, item) => ({
+    ...obj,
+    [item['key']]: item.value,
+  }), {});
 
-writeFileSync(JSON_INDEX_PATH, JSON.stringify(DATA, null, 4));
+const MERGED_DATA = set(
+  merge(
+    SETTING_DATA,
+    DATA
+  ),
+  'navigation.social',
+  values(merge(
+    keyBy(DATA.navigation.social, 'name'),
+    keyBy(NAVIGATION_DATA, 'name')
+  ))
+);
+
+writeFileSync(JSON_INDEX_PATH, JSON.stringify(MERGED_DATA, null, 2));
