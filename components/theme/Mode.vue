b26b675bfdf73663cb739f6ffc67eca6c284f66e
Eries Trisnadi <lognus21@gmail.com>
2022-07-21 15:43:25 +0700

    fix: theme mode component, using v-show instead v-if caused hydration
    Signed-off-by: Eries Trisnadi <lognus21@gmail.com>

--- components/theme/Mode.vue
+++ components/theme/Mode.vue
@@ -9,12 +9,12 @@
     "
   >
     <icon-sun
-      v-if="colorMode.preference === 'dark'"
+      v-show="colorMode.preference === 'dark'"
       class="m-[.5em] w-4 h-4 fill-current group-first:ml-[-1.7em]"
     >
     </icon-sun>
     <icon-eclipse
-      v-if="colorMode.preference === 'light'"
+      v-show="colorMode.preference === 'light'"
       class="m-[.5em] w-4 h-4"
     >
     </icon-eclipse>
